e4597c33d22c1db8c3d886b4c4fb2eb319e783b2
Naoto Murakami <naoto_murakami@terumo.co.jp>
2022-08-09 17:21:46 +0900

    second-update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { NewHomes, NavBar, MarketingFooter } from './ui-components'
 
 function App() {
-return (
-<div className="App">
-<NavBar />
-<NewHomes />
-<MarketingFooter />
-</div>
-);
+  return (
+    <div className="App">
+      <NavBar width={"100vw"}/>
+      <NewHomes isPaginated itemsPerPage={3}/>
+      <MarketingFooter width={"100vw"}/>
+    </div>
+  );
 }
 
 export default App;
